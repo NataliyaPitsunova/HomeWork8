af5869f6b7e8fc32365ed86efd62c70e5319fb0b
NataliyaPitsunova <93783392+NataliyaPitsunova@users.noreply.github.com>
2021-12-09 23:03:54 +0300

    Update MyCalc.java

--- src/main/java/MyCalc.java
+++ src/main/java/MyCalc.java
@@ -1,81 +1,128 @@
 
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicButtonListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 
-    public class MyCalc extends JFrame {
+public class Calculate {
+    public static void main(String[] args) {
+        new MyCalc();
+    }
 
+    static class MyCalc {
+        JFrame window = new JFrame("Калькулятор");
+        private JTextField screen = new JTextField();
+        private BigDecimal result;
+        private String lastAction;
+        private boolean start;
 
-        public MyCalc() {
-            setBounds(300,300,500,500);
-            setTitle("Калькулятор");
+        public MyCalc(){
+            //задаем окно
+            window.setBounds(300,300,345,345);
             Font font= new Font("Time New Romans",Font.BOLD,16);
-            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            setLayout(null);
+            window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            window.setLayout(null);
 
-            JTextField field = new JTextField();
-            field.setBounds(20,20,455,32);
-            add(field);
-            JButton [] button1_3 = new JButton [3];
-            Font fontbutton= new Font("Time New Romans",Font.BOLD,12);
-            for (int i=0;i< button1_3.length;i++) {
-                button1_3[i]=new JButton(Integer.toString(i+1));
-                button1_3[i].setBounds(20+i*60, 60, 50, 40);
-                add(button1_3[i]);
-            }
-            JButton [] button4_6 = new JButton [3];
-            for (int i=0;i< button4_6.length;i++) {
-                button4_6[i]=new JButton(Integer.toString(i+4));
-                button4_6[i].setBounds(20+i*60, 120, 50, 40);
+            //дисплей
+            screen.setBounds(20,5,295,45);
+            screen.setFont(font);
+            window.add(screen);
 
-                add(button4_6[i]);
-            }
-            JButton [] button7_9 = new JButton [3];
-            for (int i=0;i< button7_9.length;i++) {
-                button7_9[i]=new JButton(Integer.toString(i+7));
-                button7_9[i].setBounds(20+i*60, 180, 50, 40);
-
-                add(button7_9[i]);
-            }
-
-            JButton button0 =new JButton(Integer.toString(0));
-            button0.setBounds(20, 240, 80, 40);
-
-            add(button0);
-
-            JButton btnBack =new JButton("Del");
-            btnBack.setBounds(110, 240, 80, 40);
-            button0.addActionListener((ActionListener) this);
-            add(btnBack);
-
-            JButton [] buttonOperation = new JButton [4];
-            String [] signs = new String[4];
-            signs[0]="+";
-            signs[1]="-";
-            signs[2]="/";
-            signs[3]="*";
-            for (int i=0;i< buttonOperation.length;i++) {
-                buttonOperation[i]=new JButton(signs[i]);
-                buttonOperation[i].addActionListener((ActionListener) this);
-                buttonOperation[i].setBounds(200, 60+i*60, 50, 40);
-                add(buttonOperation[i]);
-            }
-            JButton btnRes =new JButton("=");
-            btnRes.setBounds(260, 60, 50, 220);
-            btnRes.addActionListener((ActionListener) this);
-            add(btnRes);
+//кнопки
+            String [] signs = {"1","2","3","+","4","5","6","-","7","8", "9","/","0","C",".","*"};
+            ActionListener numeric = new InsAction();
+            ActionListener action = new CommandAction();
+            ActionListener dell;
+            dell = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String tmp = screen.getText();
+                    if (tmp.length()>0) {
+                        screen.setText(tmp.substring(0,tmp.length()-1));}
+                }
+            };
+            ActionListener listener;
+            result = BigDecimal.ZERO;
+            lastAction = "=";
+            start = true;
+            int index=0;
+            do {
+                for (int y = 60; y <= 240; y += 60) {
+                    for (int x = 0; x < 4; x++) {
+                        if (signs[index].equals("+")||signs[index].equals("-")||signs[index].equals("*")||signs[index].equals("/")){
+                            listener=action;
+                        } else {
+                            if (!(signs[index].equals("C"))){
+                                listener=numeric;}
+                            else {listener=dell;}}
+                        addButton(signs[index], listener,x ,y);
+                        index++;
+                    }
+                }
+            } while (index < 16) ;
+            JButton eqv = new JButton("=");
+            eqv.addActionListener(action);
+            eqv.setFont(font);
+            eqv.setBounds(260, 60, 55, 235);
+            window.add(eqv);
 
 
+            window.setVisible(true);
 
-            setVisible(true);
+
+        }
+        //метод добавления кнопок
+        private void addButton(String signs, ActionListener listener,int x, int y) {
+            Font font= new Font("Time New Romans",Font.BOLD,16);
+            JButton button = new JButton(signs);
+            button.setVisible(true);
+            button.setFont(font);
+            button.addActionListener(listener);
+            button.setBounds(20 + x * 60, y, 55, 55);
+            window.add(button);
         }
 
-        public static void main(String[] args) {
-            new MyCalc();
+        //слушалка для цифр
+        private class InsAction implements ActionListener {
+            public void actionPerformed(ActionEvent event) {
+                String input = event.getActionCommand();
+                if (start) {
+                    screen.setText("");
+                    start = false;
+                }
+                String tmp = screen.getText();
+                screen.setText(tmp + input);
+            }
         }
+        //слушалка дл действий
+        private class CommandAction implements ActionListener {
+            public void actionPerformed(ActionEvent event) {
+                String action = event.getActionCommand();
+                String tmp = screen.getText();
+                if (start) {
+                    if (action.equals("-")) {
+                        screen.setText(action);
+                        start = false;
+                    } else lastAction = action;
+                } else {
+                    calculations(new BigDecimal(screen.getText()));
+                    lastAction = action;
+                    start = true;
+                }
 
-        
-
+            }
+        }
+        public void calculations(BigDecimal x) {
+            if (lastAction.equals("+")) result = result.add(x);
+            else if (lastAction.equals("-")) result = result.subtract(x);
+            else if (lastAction.equals("*")) result = result.multiply(x);
+            else if (lastAction.equals("/")) result = result.divide(x);
+            else if (lastAction.equals("=")) result = x;
+            if (result.compareTo(BigDecimal.ZERO) == 0) {
+                result = BigDecimal.ZERO;
+            }
+            screen.setText(result.toString());
+        }
+    }
 }
